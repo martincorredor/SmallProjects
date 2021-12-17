dad8872e6422dc5c77dfcc3295806dada38ccf78
Martin Corredor <dev.martincorredor@gmail.com>
2021-12-17 09:19:05 -0500

    |UPDATED| Fix some details of menu

--- bc-handicrafts/src/components/Buttons.jsx
+++ bc-handicrafts/src/components/Buttons.jsx
@@ -7,7 +7,7 @@ const Buttons = (props) => {
         BC Handicrafts
       </a>
       <div className="nav_btns">
-        <i className="uil uil-moon change-theme" id="theme-button"></i>
+        <i className="uil uil-moon change_theme" id="theme-button"></i>
         <div className="nav_toggle" id="nav-toggle">
           <i className="uil uil-apps" onClick={props.showMenu}></i>
         </div>
