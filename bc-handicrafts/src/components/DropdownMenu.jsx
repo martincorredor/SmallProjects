9f3e0f45cf6039b548bf8dc4e24e67e5eeb07b06
Martin Corredor <dev.martincorredor@gmail.com>
2021-12-17 08:36:43 -0500

    |UPDATED| Finish the menu functionalities

--- bc-handicrafts/src/components/DropdownMenu.jsx
+++ bc-handicrafts/src/components/DropdownMenu.jsx
@@ -5,27 +5,27 @@ const DropdownMenu = (props) => {
     <div className="drop_menu" id="drop-menu">
       <ul className="drop_list">
         <li className="drop_item">
-          <a href="#home" className="drop_link">
+          <a href="#home" className="drop_link" onClick={props.hideMenu}>
             <i className="uil uil-estate drop_icon"></i> Home
           </a>
         </li>
         <li className="drop_item">
-          <a href="#about" className="drop_link">
+          <a href="#about" className="drop_link" onClick={props.hideMenu}>
             <i className="uil uil-user drop_icon"></i> About
           </a>
         </li>
         <li className="drop_item">
-          <a href="#shop" className="drop_link">
+          <a href="#shop" className="drop_link" onClick={props.hideMenu}>
             <i className="uil uil-shopping-cart drop_icon"></i> Shop
           </a>
         </li>
         <li className="drop_item">
-          <a href="#comments" className="drop_link">
+          <a href="#comments" className="drop_link" onClick={props.hideMenu}>
             <i className="uil uil-comments-alt drop_icon"></i> Comments
           </a>
         </li>
         <li className="drop_item">
-          <a href="#session" className="drop_link">
+          <a href="#session" className="drop_link" onClick={props.hideMenu}>
             <i className="uil uil-user drop_icon"></i> Session
           </a>
         </li>
